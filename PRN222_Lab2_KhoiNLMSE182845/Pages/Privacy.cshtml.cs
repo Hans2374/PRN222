40c82dc7ad15b4ffa0f718fcadd8667c08a87af2
Hans2374 <minhkhoi23704@gmail.com>
2025-05-17 11:49:23 +0700

    Initial commit

---
+++ PRN222_Lab2_KhoiNLMSE182845/Pages/Privacy.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PRN222_Lab2_KhoiNLMSE182845.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+
+}
---
+++ PharmacyWebApp.Data/Repositories/StoreAccountRepository.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PharmacyWebApp.Data.Models;
+using System.Threading.Tasks;
+namespace PharmacyWebApp.Data.Repositories
+{
+    public class StoreAccountRepository : IStoreAccountRepository
+    {
+        private readonly Lab2PharmaceuticalDbContext _context;
+        public StoreAccountRepository(Lab2PharmaceuticalDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<StoreAccount> GetByEmailAndPasswordAsync(string email, string password)
+        {
+            return await _context.StoreAccounts
+            .FirstOrDefaultAsync(a => a.EmailAddress == email
+            && a.StoreAccountPassword == password);
+        }
+    }
+}
